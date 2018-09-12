2b0cfad4e14fba97a89e09d95bb9df27ff7cea2a
royWati <royywati@gmail.com>
2018-09-12 09:05:05 +0300

    origin

---
+++ app/src/main/java/com/abdihakim/loginapp/RadiosAndCheckBoxes.java
@@ -0,0 +1,68 @@
+package com.abdihakim.loginapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class RadiosAndCheckBoxes extends AppCompatActivity {
+
+    RadioGroup radioGroupGender;
+    RadioButton rd_female,rd_male;
+
+    CheckBox ch_email,ch_sms,ch_push;
+
+    @Override
+    protected void onCreate(Bundle bundle){
+        super.onCreate(bundle);
+        setContentView(R.layout.radios_and_checkboxes);
+        setTitle(R.string.radio_and_checkboxes_title);
+
+        radioGroupGender=findViewById(R.id.group_gender);
+        rd_female=findViewById(R.id.radio_female);
+        rd_male=findViewById(R.id.radio_male);
+
+        ch_email=findViewById(R.id.ch_get_emails);
+        ch_sms=findViewById(R.id.ch_sms);
+        ch_push=findViewById(R.id.ch_enable_push);
+
+        radios();
+
+        checks(ch_email,"email notification");
+        checks(ch_sms,"Sms notification");
+        checks(ch_push,"Push notification");
+    }
+
+    public void radios(){
+
+        radioGroupGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(rd_female.isChecked())
+                    Toast.makeText(getApplicationContext(),
+                            "Female has been checked",Toast.LENGTH_SHORT).show();
+                else if(rd_male.isChecked())
+                    Toast.makeText(getApplicationContext(),
+                            "Male has been checked",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void checks(CheckBox checkBox, final String notification_type){
+
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(((CheckBox)v).isChecked())Toast.makeText(getApplicationContext(),notification_type+" is enabled",
+                        Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(),notification_type+" is disabled",
+                            Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/abdihakim/loginapp/registration.java
@@ -0,0 +1,49 @@
+package com.abdihakim.loginapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class registration extends AppCompatActivity {
+    EditText ed_name, ed_email, ed_password, ed_confpass;
+    Button ed_register;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        ed_name = findViewById(R.id.name);
+        ed_email = findViewById(R.id.email);
+        ed_password = findViewById(R.id.password);
+        ed_confpass = findViewById(R.id.confirmpass);
+        ed_register = findViewById(R.id.button2);
+
+        ed_register.setOnClickListener(new View.OnClickListener() {
+     @Override
+     public void onClick(View v) {
+         String pass= ed_password.getText().toString();
+         String conf = ed_confpass.getText().toString();
+
+
+         if(pass.equals(conf)){
+
+             Toast.makeText(getApplicationContext(),"Password match",Toast.LENGTH_SHORT).show();
+
+             Intent intentObj=
+                     new Intent(registration.this,RadiosAndCheckBoxes.class);
+             startActivity(intentObj);
+         }
+         else{
+
+            Toast.makeText(getApplicationContext(),"Password dont match",Toast.LENGTH_SHORT).show();
+         }
+     }
+ });
+    }
+}
