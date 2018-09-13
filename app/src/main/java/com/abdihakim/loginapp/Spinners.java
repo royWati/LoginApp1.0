ebb0d70c66908f08f7700ac365cb98c4a78bcdd7
royWati <royywati@gmail.com>
2018-09-13 10:36:40 +0300

    origin-master

--- app/src/main/java/com/abdihakim/loginapp/Spinners.java
+++ app/src/main/java/com/abdihakim/loginapp/Spinners.java
@@ -1,5 +1,6 @@
 package com.abdihakim.loginapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -15,6 +17,8 @@ public class Spinners extends AppCompatActivity{
     String[] names = {"Hakim","Joel","Chege","Newton","Dennis"};
     ArrayAdapter adapter;
     Spinner spinnerObj;
+
+    Button btn_next;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +27,15 @@ public class Spinners extends AppCompatActivity{
         adapter = new ArrayAdapter(Spinners.this,android.R.layout.simple_list_item_1,names);
         adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
         spinnerObj.setAdapter(adapter);
+        btn_next=findViewById(R.id.btn_next);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Spinners.this,Container.class);
+                startActivity(intent);
+            }
+        });
 
         spinnerObj.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
